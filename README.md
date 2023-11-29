4f71c7ec22e951a095f8327e120de3be61790064
Leandro Andrekowicz <103007824+LeandroAndrekowicz@users.noreply.github.com>
2023-11-29 17:50:25 -0300

    Criei novo componente comentarios e ajustes na frase

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
+
 # primeiro_flutter
 
 A new Flutter project.
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:percent_indicator/linear_percent_indicator.dart';
 
 void main() {
   runApp(meuApp());
@@ -8,6 +9,7 @@ class meuApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         drawer: Drawer(
           // Add a ListView to the drawer. This ensures the user can scroll
@@ -27,7 +29,7 @@ class meuApp extends StatelessWidget {
               ),
               ListTile(
                 title: const Text('Sugerir frase'),
-                trailing: Icon(Icons.add),
+                trailing: const Icon(Icons.add),
                 onTap: () {
                   // Update the state of the app.
                   // ...
@@ -42,10 +44,10 @@ class meuApp extends StatelessWidget {
               ),
               ListTile(
                 title: const Text('Editar meu Perfil'),
-                shape: Border(
+                shape: const Border(
                     bottom: BorderSide(
                         color: Color.fromARGB(255, 0, 0, 0), width: 1)),
-                trailing: Icon(Icons.person_pin),
+                trailing: const Icon(Icons.person_pin),
                 onTap: () {
                   // Update the state of the app.
                   // ...
@@ -53,13 +55,13 @@ class meuApp extends StatelessWidget {
               ),
               ListTile(
                   title: const Text('Sair'),
-                  trailing: Icon(Icons.power_settings_new),
+                  trailing: const Icon(Icons.power_settings_new),
                   onTap: () {})
             ],
           ),
         ),
         appBar: AppBar(
-          backgroundColor: Color.fromARGB(255, 255, 255, 255),
+          backgroundColor: const Color.fromARGB(255, 255, 255, 255),
           actions: [
             IconButton(
               icon: Image.asset(
@@ -68,46 +70,190 @@ class meuApp extends StatelessWidget {
               onPressed: () {},
             )
           ],
-          title: Text('Frase da semana'),
+          title: const Text('Frase da semana'),
           centerTitle: true,
         ),
-        body: frases(),
+        body: Column(
+          children: [
+            const frases(),
+            comentarios(),
+          ],
+        ),
       ),
     );
   }
 }
 
-Widget frases() {
-  return (MaterialApp(
-    home: Scaffold(
-      body: SafeArea(
-          left: true,
-          right: false,
-          top: false,
-          bottom: true,
-          child: FractionallySizedBox(
-              widthFactor: 0.8,
-              heightFactor: 0.5,
-              alignment: Alignment.center,
+// ignore: camel_case_types
+class frases extends StatelessWidget {
+  const frases({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: MediaQuery.of(context).size.width,
+      height: 450,
+      decoration: BoxDecoration(
+          borderRadius: const BorderRadius.all(Radius.circular(10)),
+          border: Border.all(
+            color: Colors.grey,
+            width: 0.5,
+          )),
+      child: Center(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              SizedBox(height: MediaQuery.of(context).padding.top),
+              const SizedBox(height: 12.0),
+              const Text(
+                "Negociação de Sucesso",
+                textAlign: TextAlign.center,
+                style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),
+              ),
+              const SizedBox(height: 16.0),
+              const Padding(
+                padding: EdgeInsets.symmetric(horizontal: 8.0),
+                child: Text(
+                  'A apresentação da proposta personalizada para o cliente é o nosso diferencial para fechamento do negócio.',
+                  textAlign: TextAlign.justify,
+                ),
+              ),
+              const SizedBox(height: 16.0),
+              const Padding(
+                padding: EdgeInsets.symmetric(horizontal: 8.0),
+                child: Text(
+                  'Quando fazemos uma proposta para o cliente até o fechamento leva-se em média 40 dias.',
+                  textAlign: TextAlign.justify,
+                ),
+              ),
+              const SizedBox(height: 16.0),
+              const Padding(
+                padding: EdgeInsets.symmetric(horizontal: 8.0),
+                child: Text(
+                  'Devido a isso é tão importante ter um momento com o cliente onde devemos reforçar toda consultoria feita em obra, debater os detalhes dos ambientes e reforçar todos os benefícios do produto.',
+                  textAlign: TextAlign.justify,
+                ),
+              ),
+              const SizedBox(height: 16.0),
+              const Padding(
+                padding: EdgeInsets.symmetric(horizontal: 8.0),
+                child: Text(
+                  'Com isso, além de lembrar o cliente do que fizemos passamos segurança e autoridade para finalizar a venda',
+                  textAlign: TextAlign.justify,
+                ),
+              ),
+              const SizedBox(height: 16.0),
+              const Padding(
+                padding: EdgeInsets.symmetric(horizontal: 8.0),
+                child: Text(
+                  'O que acha disso? Conte uma experiência positiva que já teve.',
+                  textAlign: TextAlign.justify,
+                ),
+              ),
+              const SizedBox(
+                height: 12.0,
+              ),
+              const Padding(
+                padding: EdgeInsets.symmetric(horizontal: 8.0),
+                child: Text(
+                  'Por: João Ricardo Olinquevicz.',
+                  textAlign: TextAlign.right,
+                  style: TextStyle(fontSize: 10),
+                ),
+              ),
+              const SizedBox(height: 16.0),
+              LinearPercentIndicator(
+                width: MediaQuery.of(context).size.width - 45,
+                animation: true,
+                animationDuration: 1000,
+                lineHeight: 5,
+                percent: 0.94,
+                progressColor: Colors.blue,
+              ),
+              const SizedBox(height: 16.0),
+              const SizedBox(height: 16.0),
+              const Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 20),
+                  child: Row(
+                    textDirection: TextDirection.rtl,
+                    children: [
+                      Icon(
+                        Icons.circle,
+                        color: Colors.orange,
+                      ),
+                      Icon(
+                        Icons.circle,
+                        color: Colors.green,
+                      ),
+                      Icon(
+                        Icons.circle,
+                        color: Colors.green,
+                      ),
+                      Icon(
+                        Icons.circle,
+                        color: Colors.green,
+                      ),
+                    ],
+                  ))
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class comentarios extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: MediaQuery.of(context).size.width,
+      height: 450,
+      decoration: BoxDecoration(
+        borderRadius: const BorderRadius.all(Radius.circular(10)),
+        border: Border.all(
+          color: Colors.grey,
+          width: 0.5,
+        ),
+      ),
+      child: const Padding(
+        padding: EdgeInsets.symmetric(horizontal: 20.0),
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Expanded(
               child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
+                  SizedBox(height: 16.0),
                   Text(
-                    'Negociação de Sucesso',
-                    style: TextStyle(fontSize: 20),
+                    'Teste testilson',
+                    style: TextStyle(fontWeight: FontWeight.bold),
                   ),
+                  SizedBox(height: 12.0),
                   Text(
-                    'A apresentação da proposta personalizada para o cliente é o nosso diferencial para fechamento do negócio.',
-                  ),
-                  Text(
-                      'Quando fazemos uma proposta para o cliente até o fechamento leva-se em média 40 dias.'),
-                  Text(
-                      'Devido a isso é tão importante ter um momento com o cliente onde devemos reforçar toda consultoria feita em obra, debater os detalhes dos ambientes e reforçar todos os beneficios do produto.'),
-                  Text(
-                      'Com isso, além de lembrar o cliente do que fizemos passamos segurança e autoridade para finalizar a venda'),
-                  Text(
-                      'O que acha disso? Conte uma experiência positiva que já teve.')
+                    'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using Content here, content here, making it look like',
+                    textAlign: TextAlign.justify,
+                  )
                 ],
-              ))),
-    ),
-  ));
+              ),
+            ),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.end,
+              children: [
+                SizedBox(height: 16.0),
+                Align(
+                  alignment: Alignment.bottomCenter,
+                  child: Icon(Icons.favorite_border),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
 }
